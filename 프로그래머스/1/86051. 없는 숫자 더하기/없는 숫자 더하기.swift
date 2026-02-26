b0d651c3d0ca99e4bec399e652e1a9c237e24678
Jade <a_jb97@naver.com>
2026-02-26 15:49:18 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> Int {
+    let allNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let filteredNumbers = allNumbers.filter {
+        !numbers.contains($0)
+    }
+    var result = 0
+    
+    filteredNumbers.forEach { result += $0 }
+    
+    return result
+}
