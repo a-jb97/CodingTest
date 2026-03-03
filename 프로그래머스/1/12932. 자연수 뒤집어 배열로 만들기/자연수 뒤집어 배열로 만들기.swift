70b9d9184e25d4678b133e9dee140a9e26f2f5a6
Jade <a_jb97@naver.com>
2026-03-03 16:20:02 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
@@ -1,23 +1,12 @@
 import Foundation
 
 func solution(_ n:Int64) -> [Int] {
-    var currentIndex = String(n).count - 1
-    var currentN = Int(n)
-    var result: [Int] = []
-    
-    if currentIndex == 0 {
-        return [Int(n)]
-        
-    } else {
-        for _ in 0...currentIndex {
-            var digit = Int(pow(10.0, Double(currentIndex)))
-            
-            result.insert(Int(currentN) / digit, at: 0)
-            
-            currentN -= (Int(currentN) / digit) * digit
-            currentIndex -= 1
-        }
-        
-        return result
+    var num: Int = Int(n)
+    var arr: [Int] = []
+
+    while num > 0 {
+        arr.append(num % 10)
+        num /= 10
     }
+    return arr
 }
