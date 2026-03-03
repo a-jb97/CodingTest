9a2423fe5ef7dba9f0cc0ab1d9a6c25310f17125
Jade <a_jb97@naver.com>
2026-03-03 14:39:49 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    let total = brown + yellow
+    
+    for height in 1...total {
+        if total % height == 0 {
+            let width = total / height
+            
+            if width >= height {
+                if (width - 2) * (height - 2) == yellow {
+                    return [width, height]
+                }
+            }
+        }
+    }
+    
+    return []
+}
