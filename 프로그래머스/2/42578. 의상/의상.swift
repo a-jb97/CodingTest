569ce855691dd2c25eba6464b4315591e5de2d0b
Jade <a_jb97@naver.com>
2026-03-03 20:01:13 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.swift
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 func solution(_ clothes:[[String]]) -> Int {
-    return 0
+    var dict: [String: Int] = [:]
+    var result = 1
+    
+    for cloth in clothes {
+        let category = cloth[1]
+        
+        dict[category, default: 0] += 1
+    }
+    
+    for count in dict.values {
+        result *= (count + 1)
+    }
+    
+    return result - 1
 }
