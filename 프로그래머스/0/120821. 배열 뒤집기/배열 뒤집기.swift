0563bd783a430705f534f88a6d2abbd828f1f883
Jade <a_jb97@naver.com>
2026-03-03 15:24:10 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.swift
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.swift
@@ -1,11 +1,6 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    var result: [Int] = []
     
-    for num in num_list {
-        result.insert(num, at: 0)
-    }
-    
-    return result
+    return num_list.reversed()
 }
