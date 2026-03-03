a7cb72d0cf3c69b83e356763d1394a6b72fb90ef
Jade <a_jb97@naver.com>
2026-03-03 11:11:48 +0900

    [level 2] Title: 짝지어 제거하기, Time: 387.94 ms, Memory: 21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ s:String) -> Int{
+    var stack: [Character] = []
+    
+    for char in s {
+        if stack.last == char {
+            stack.removeLast()
+        } else {
+            stack.append(char)
+        }
+    }
+    
+    return stack.isEmpty ? 1 : 0
+}
