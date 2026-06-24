1824a7ed5b130358843ea999415dd6eb0407da91
Jade <a_jb97@naver.com>
2026-06-24 23:06:15 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 20.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.swift
@@ -0,0 +1,7 @@
+func solution(_ s:String) -> String {
+    let numbers = s.split(separator: " ").compactMap { Int($0) }
+    let minValue = numbers.min()!
+    let maxValue = numbers.max()!
+
+    return "\(minValue) \(maxValue)"
+}
